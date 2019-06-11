8af8557f8acacefb7f81043b05a49434f04ccb30
LUYU0004 <luyuhao00@gmail.com>
2019-06-11 22:22:38 +0800

    minor update

--- scripts/test_PAM_regression_repeats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH -o PAM-regression-%j.output
-#SBATCH -p RTXq
-#SBATCH -n 4
-export PATH=/home/yuhao001/anaconda3/envs/mos2_project/bin:$PATH
-cd /home/yuhao001/projects/ML-guided-material-synthesis/
-python3 PAM-regression_repeat1000times.py
